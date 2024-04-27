d2bdc89603fff193ad924ba389dc49741b137624
asus test <msa-99@outlook.com>
2024-04-27 20:18:32 +0600

    count only consonants

--- contest.c
+++ contest.c
@@ -1,19 +1,4 @@
-// //problem 2: count only consonants
-// #include<stdio.h>
-// #include<limits.h>
-// #include<string.h>
-// int main(){
-//     char s[100001];
-//     scanf("%s",&s);
-//     int totalCons=0;
-//     for(int i=0; i<strlen(s); i++){
-//         if(s[i]!='a' && s[i]!='e' && s[i]!='i' && s[i]!='o' && s[i]!='u'){
-//             totalCons++;
-//         }
-//         }
-//         printf("%d",totalCons);
-//     return 0;
-// }
+
 
 // //problem 4: calculate work days
 // #include<stdio.h>
