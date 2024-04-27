838ee954e048d2783117b8f268708d100c3b73b9
asus test <msa-99@outlook.com>
2024-04-27 20:26:54 +0600

    count letters

--- contest.c
+++ contest.c
@@ -30,29 +30,6 @@
 //     return 0;
 // }
 
-
-
-// //problem 6: count letters
-// #include<stdio.h>
-// #include<limits.h>
-// #include<string.h>
-// int main(){
-//     char s[10001];
-//     scanf("%s",&s);
-//     int len=strlen(s);
-//     int fqArr[26]={0};
-//     for(int i=0; i<=len; i++){
-//       int val=s[i]-97;
-//        fqArr[s[i]-97]++;
-//     }
-//     for(int i=0; i<26; i++){
-//         if(fqArr[i]>0){
-//             printf("%c - %d\n",i+97, fqArr[i]);
-//         }
-//     }
-//     return 0;
-// }
-
 // //problem 7: exist or not
 // #include<stdio.h>
 // #include<limits.h>
